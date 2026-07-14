1ba8cd97bcac7efffb102469b909e7a0b0fe5e26
Tropilacc <jamiemeechan@icloud.com>
2026-07-14 14:32:51 +1000

    Initial project setup

---
+++ app/main.py
@@ -0,0 +1,14 @@
+print("===================================")
+print(" Greyhound Price Monitor")
+print("===================================")
+
+DOG_NAME = "Fast Fred"
+
+INITIALPRICE = 4.60
+
+CURRENTPRICE = 10.80
+
+print()
+print("Dog:", DOG_NAME)
+print("Initial Price:", INITIALPRICE)
+print("Current Price:", CURRENTPRICE)
