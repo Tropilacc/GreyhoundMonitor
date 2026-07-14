93d935adfa9eca3e2ba4200f59fbe867e5ca0234
Tropilacc <jamiemeechan@icloud.com>
2026-07-14 14:35:20 +1000

    Move project files to root

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
-print("===================================")
-print(" Greyhound Price Monitor")
-print("===================================")
-
 DOG_NAME = "Fast Fred"
 
 INITIALPRICE = 4.60
 
 CURRENTPRICE = 10.80
 
-print()
-print("Dog:", DOG_NAME)
-print("Initial Price:", INITIALPRICE)
-print("Current Price:", CURRENTPRICE)
+if INITIALPRICE < 5 and CURRENTPRICE > 10:
+
+    print("********************************")
+    print("ALERT")
+    print(DOG_NAME)
+    print("Initial:", INITIALPRICE)
+    print("Current:", CURRENTPRICE)
+    print("********************************")
+
+else:
+
+    print("No alert")
